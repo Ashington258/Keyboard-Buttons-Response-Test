3b73db118710a011e25381dcf45f579c491ea017
ThreeSwords Leaf <921488837@qq.com>
2023-07-02 19:40:28 +0800

    更改了文件结构

--- include/function.h
+++ include/function.h
@@ -2,9 +2,16 @@
 #define _FUNCTION_H
 
 
+/*
+用于返回按下按键时的时间戳
 
-//
 
+*/
+LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam);
+
+HHOOK SetKeyboardHook();
+
+void MessageLoop(HHOOK hook);
 
 #endif // !_FUNCTION_H
 
